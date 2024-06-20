98110116e78aeebf5fd92f79ee4c80c6dfa72308
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-06-20 17:20:40 -0300

    2 primeiras funcoes para turma de qualificacao

--- exercicio01/script.js
+++ exercicio01/script.js
@@ -1,5 +1,18 @@
+function somar(){
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    var res = document.getElementById("resposta");
+    res.textContent = num1 + num2;
+}
+
+function sub(){
+    var n1 = document.getElementById("n1").valueAsNumber;
+    var n2 = document.getElementById("n2").valueAsNumber;
+    var resp = document.getElementById("res");
+    resp.innerHTML = n1 - n2;
+}
 
 
-var animal = "lobo";
 
-alert(animal)
+
+
